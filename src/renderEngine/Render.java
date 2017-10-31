97fcee421a3ce97110c4b6a2a0f5b5ff8a1e86f2
Fabian <fadub@hotmail.com>
2017-10-31 22:30:34 +0100

    initial commit

---
+++ src/renderEngine/Render.java
@@ -0,0 +1,18 @@
+package renderEngine;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
+public class Render {
+
+	public void prepare() {
+		GL11.glClearColor(1, 0, 0, 1);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+	}
+	
+	public void render(RawModel model) {
+		GL30.glBindVertexArray(model.getVaoID());
+		GL20.glEnableVertexAttribArray(0);
+	}
+}
